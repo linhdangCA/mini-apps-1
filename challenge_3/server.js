f5b83577ef0bc7f62ddedae6924b1b6cd9336dd2
Linh Dang <linh.dang01@gmail.com>
2020-11-02 14:11:15 -0800

    Added generic server connection with express

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+var express = require('express');
+var app = express();
 
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+app.listen(port, () => {
+  console.log('listening to port: ', port);
+})
