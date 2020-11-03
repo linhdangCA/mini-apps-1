7562747b11fa6c52c7b879775d855242881fc2d3
Linh Dang <linh.dang01@gmail.com>
2020-11-03 11:59:53 -0800

    connected app src to html added in dynamic rendering capability for current player

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,23 @@
 // ------------ model ---------------
 // build the board
-// render the placed pick
+// player x positions
+var playerXPicks = [];
+// player o positions
+var playerOPicks = [];
+// winning combinations
+var winningCombinations = [
+  [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]
+];
+// spots that have already been picked
+var unavailablePicks = [];
 // show who's turn it is
+var currentPlayer = 'X';
+
+// render the placed pick
 // render winner or tie
 
 
-
-
-
 // ----------- controller -----------
-// first player is X
 // logic for placing pick
   // check for conflict
 // swap to other player's turn
@@ -22,6 +30,7 @@
 
 
 // ------------- view ---------------
+// show current player
 // build event listener for placing player's pick
 // button to reset the board
 
