0357fa82ca63aed2d19c624e8b52fed0f704f904
Linh Dang <linh.dang01@gmail.com>
2020-11-02 20:11:39 -0800

    Added next button, userInfo, and ShippingInfo pages

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -7,10 +7,15 @@ class App extends React.Component {
       email: '',
       password: '',
       shipping_id: null,
-      billing_id: null
+      billing_id: null,
+      address1: '',
+      address2: '',
+      city: '',
+      state: '',
+      zipcode: null
     };
 
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNext = this.handleNext.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -18,11 +23,24 @@ class App extends React.Component {
     this.setState({currentPage: 'userInfo'});
   }
 
-  // handleSubmit() {
+  handleNext(e) {
+    console.log(this)
+    e.preventDefault();
+    if (this.state.currentPage === 'homepage') {
+      this.setState({currentPage: 'userInfo'})
+    } else if (this.state.currentPage === 'userInfo') {
+      this.setState({currentPage: 'shippingInfo'})
+    } else if (this.state.currentPage === 'shippingInfo') {
+      this.setState({currentPage: 'billingInfo'})
+    } else if (this.state.currentPage === 'billingInfo') {
+      this.setState({currentPage: 'confirmationPage'})
+    } else {
+      this.setState({currentPage: 'homepage'})
+    }
+  }
 
-  // }
-
-  handleChange(event) {
+  handleChange(e) {
+    e.preventDefault();
     const target = event.target;
     const value = target.value;
     const name = target.name;
@@ -40,16 +58,16 @@ class App extends React.Component {
       page = <Homepage />;
     // form 1
     } else if (currentPage === 'userInfo') {
-      page = <UserInfo name={this.state.value} handleChange={this.handleChange}/>
+      page = <UserInfo handleNext={this.handleNext} handleChange={this.handleChange}/>
     // form 2
     } else if (currentPage === 'shippingInfo') {
-      page = <ShippingInfo />
+      page = <ShippingInfo handleNext={this.handleNext} handleChange={this.handleChange}/>
     // form 3
     } else if (currentPage === 'billingInfo') {
-      page = <BillingInfo />
+      page = <BillingInfo handleNext={this.handleNext} handleChange={this.handleChange}/>
     // confirmation page
-    } else if (currentPage === 'confirmation' ) {
-      page = <Confirmation />
+    } else if (currentPage === 'confirmationPage' ) {
+      page = <ConfirmationPage handleNext={this.handleNext} handleChange={this.handleChange}/>
     }
 
     return (
@@ -63,7 +81,7 @@ function Homepage() {
 }
 function UserInfo(props) {
   return (
-    <form>
+    <form onSubmit={()=>props.handleNext(event)}>
       <h1>Account creation</h1>
       <label>Name:
         <input name="name" type="text" onChange={()=>props.handleChange(event)}></input>
@@ -77,17 +95,42 @@ function UserInfo(props) {
         <input name="password" type="password" onChange={()=>props.handleChange(event)}></input>
       </label>
       <br />
-      <input type="submit" value="Next"></input>
+      <button>Next</button>
     </form>
   )
 }
-function ShippingInfo() {
+function ShippingInfo(props) {
+  return (
+    <form onSubmit={()=>props.handleNext(event)}>
+      <h1>Shipping Information</h1>
+      <label>Address (Line 1):
+        <input name="address1" type="text" onChange={()=>props.handleChange(event)}></input>
+      </label>
+      <br />
+      <label>Address (Line 2):
+        <input name="address2" type="text" onChange={()=>props.handleChange(event)}></input>
+      </label>
+      <br />
+      <label>City:
+        <input name="city" type="password" onChange={()=>props.handleChange(event)}></input>
+      </label>
+      <br />
+      <label>State:
+        <input name="state" type="password" onChange={()=>props.handleChange(event)}></input>
+      </label>
+      <br />
+      <label>Zipcode:
+        <input name="zipcode" type="password" onChange={()=>props.handleChange(event)}></input>
+      </label>
+      <br />
+      <button>Next</button>
+    </form>
+  )
+}
+function BillingInfo(props) {
   return (<button>NEXT</button>)
 }
-function BillingInfo() {
-  return (<button>NEXT</button>)
-}
-function Confirmation() {
+function confirmationPage(props) {
   return (<button>NEXT</button>)
 }
 
