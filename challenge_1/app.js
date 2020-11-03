a1d9342c88523d6f993fa241a853f477d1cac4aa
Linh Dang <linh.dang01@gmail.com>
2020-11-03 11:29:04 -0800

    added in html skeleton and initial notes for model, controller, view

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,30 @@
+// ------------ model ---------------
+// build the board
+// render the placed pick
+// show who's turn it is
+// render winner or tie
+
+
+
+
+
+// ----------- controller -----------
+// first player is X
+// logic for placing pick
+  // check for conflict
+// swap to other player's turn
+// logic for winner and tie
+  // map winning combinations combinations
+  // tie if all 9 spots are filled and no winner
+// functionality to button to reset the board
+
+
+
+
+// ------------- view ---------------
+// build event listener for placing player's pick
+// button to reset the board
+
+
 
 
